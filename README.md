4d5e6fa1f8d6d033bf96b18a67d5c59d89246bfa
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-09-29 20:41:50 +0700

    MallHub Flutter Initialized

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_mallhub
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_mallhub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_mallhub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
